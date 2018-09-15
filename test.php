ea1120548004349d194b8199dd0a8ad2fcc073d6
biimm <be4mz@hotmail.com>
2018-09-15 15:09:45 +0700

    first commit

---
+++ test.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>ภาควิชาคอมพิวเตอร์ มหาวิทยาลัยศิลปากร</title>
+<meta charset="utf-8">
+</head>
+<body>
+<br><br><br><br><br><br>
+<center><img src="IMG/login.png"  width="150" height="150">
+<p style="font-size:160%;">Login</p>
+<form action="/action_page.php">
+  Username:
+  <input type="text" name="firstname" value="">
+  <br><br>
+  Password:
+  <input type="text" name="lastname" value="">
+  <br><br>
+  <input type="submit" value="Submit">
+  <form><br><br>
+  <input type="radio" name="Keep me signed in" value="Keep me signed in">Keep me signed in<ul></ul>
+  <li><a href="https://www.su.ac.th/" target="_blank">Forgot my password</a></li>
+</form></center>
+</form> 
+
+</body>
+</html>
